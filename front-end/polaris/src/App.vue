736312a7b242f2bda0d0e30523c42b7e83db8fcf
조서현 <westblack@naver.com>
2024-06-11 16:07:29 +0900

    POLARIS

---
+++ front-end/polaris/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header></Header>
+  <div class="col-span-12 mt-20">
+    <RouterView/>
+  </div>
+  <Footer></Footer>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/common/Header.vue'
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ front-end/polaris/src/components/essay/EssayList.vue
@@ -0,0 +1,12 @@
+<template>
+  <EssayListItem v-for="(essay, index) in props.essayList" :key="index" :essayInfo="essay" />
+</template>
+
+<script setup lang="ts">
+import EssayListItem from './EssayListItem.vue'
+
+const props = defineProps(['essayList'])
+</script>
+
+<style scoped>
+</style>
